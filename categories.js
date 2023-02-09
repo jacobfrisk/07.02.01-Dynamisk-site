5f87d69163396b8f737b79e41885a87e977f486e
Jacob Friis Strand <jacobfriisstrand@gmail.com>
2023-02-09 10:54:07 +0100

    minified

--- categories.js
+++ categories.js
@@ -1,18 +1,1 @@
-fetch("http://kea-alt-del.dk/t7/api/categories")
-  .then((res) => res.json())
-  .then(showCategories);
-
-const template = document.querySelector("#category-button").content;
-const container = document.querySelector(".category");
-
-function showCategories(data) {
-  data.forEach((category) => {
-    const clone = template.cloneNode(true);
-    clone.querySelector("a").textContent = category.category;
-    clone.querySelector("a").href = `shop.html?category=${category.category}`;
-    container.appendChild(clone);
-  });
-}
-
-const shopAllLink = document.querySelector("#shop-all");
-shopAllLink.href = "shop.html";
+fetch("http://kea-alt-del.dk/t7/api/categories").then((e=>e.json())).then(showCategories);const template=document.querySelector("#category-button").content,container=document.querySelector(".category");function showCategories(e){e.forEach((e=>{const t=template.cloneNode(!0);t.querySelector("a").textContent=e.category,t.querySelector("a").href=`shop.html?category=${e.category}`,container.appendChild(t)}))}const shopAllLink=document.querySelector("#shop-all");shopAllLink.href="shop.html";
